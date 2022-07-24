b5e3e6ba8744b879c2e92d35413f0fc76fa4f7e8
GyanendraMani <gyanendramanimishra@gmail.com>
2022-07-24 16:50:57 +0530

    initial commit

---
+++ src/components/SummaryCard.js
@@ -0,0 +1,19 @@
+import moment from 'moment'
+
+function SummaryCard({ day, REACT_APP_ICON_URL }) {
+    let day_icon = `${REACT_APP_ICON_URL + day.weather[0]["icon"]}@2x.png`
+    return (
+        <li className="container p-4 flex items-center justify-center bg-gray-200 rounded-lg my-auto mr-1">
+            <div className="my-auto">
+                <p className="font-bold text-3xl text-pink-600 mb-2">{Math.round(day.main.temp)}&deg;C</p>
+                <p className="text-2xl text-gray-800 tracking-widest">{day.weather[0].main}
+                    <img src={day_icon} alt="" className="w-1/4 inline" />
+                </p>
+                <p className="text-gray-400 text-xs uppercase tracking-widest">{day.weather[0].description}</p>
+                <p className="tracking-wider">{moment(day.dt_txt).format("dddd hh:mm")}am</p>
+            </div>
+        </li>
+    )
+}
+
+export default SummaryCard
